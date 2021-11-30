32b9536b5e47538cf6b181efdacd4ae572450c02
oykwon <oykwon@rsupport.com>
2021-12-01 00:02:00 +0900

    4-1 commit

--- week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/AnimatingCircleTests.kt
+++ week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/AnimatingCircleTests.kt
@@ -21,13 +21,13 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.size
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.test.ExperimentalTestApi
-import androidx.compose.ui.test.MainTestClock
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onRoot
 import androidx.compose.ui.unit.dp
 import androidx.test.filters.SdkSuppress
 import com.example.compose.rally.ui.components.AnimatedCircle
+import com.example.compose.rally.ui.components.RallyTopAppBar
+import com.example.compose.rally.ui.overview.OverviewBody
 import com.example.compose.rally.ui.theme.RallyTheme
 import org.junit.Rule
 import org.junit.Test
@@ -49,6 +49,67 @@ class AnimatingCircleTests {
     @get:Rule
     val composeTestRule = createComposeRule()
 
+    @Test
+    fun rallyTopAppBarTest() {
+        val allScreens = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = { },
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+        Thread.sleep(5000)
+    }
+
+    @Test
+    fun rallyTopAppBarTest_currentTabSelected() {
+        val allScreens = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = { },
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+
+        composeTestRule
+            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .assertIsSelected()
+    }
+
+    @Test
+    fun rallyTopAppBarTest_currentLabelExists() {
+        val allScreens = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = { },
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+
+        composeTestRule.onRoot(useUnmergedTree = true).printToLog("currentLabelExists")
+        composeTestRule.onNode(
+            hasText(RallyScreen.Accounts.name.uppercase()) and
+                    hasParent(
+                        hasContentDescription(RallyScreen.Accounts.name)
+                    ),
+            useUnmergedTree = true
+        ).assertExists()
+    }
+
+
+    @Test
+    fun overviewScreen_alertsDisplayed() {
+        composeTestRule.setContent {
+            OverviewBody()
+        }
+
+        composeTestRule
+            .onNodeWithText("Alerts")
+            .assertIsDisplayed()
+    }
     @Test
     fun circleAnimation_idle_screenshot() {
         composeTestRule.mainClock.autoAdvance = true
